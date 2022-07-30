1e6cccecac22c0d101ba455e0f3f8d774da72c2a
Fabio <fabio.brambilla1998@gmail.com>
2022-07-30 17:48:44 +0200

    Added icon and fixed bugs

--- lib/ui/Quiz/quiz_page.dart
+++ lib/ui/Quiz/quiz_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:quizz_app/models/question.dart';
@@ -6,7 +5,131 @@ import 'package:quizz_app/ui/Game/game.dart';
 import 'package:quizz_app/ui/Quiz/cubit/quizz_cubit.dart';
 import 'package:quizz_app/utils/extensions.dart';
 
-class QuizzPage extends StatelessWidget {
+class QuizzPage extends StatefulWidget {
+  final List<Question> questions;
+  final cubit = QuizzCubit();
+  final numberController = TextEditingController();
+  bool maxPressed = false;
+  QuizzPage({Key? key, required this.questions}) : super(key: key);
+
+  @override
+  State<QuizzPage> createState() => _QuizzPageState();
+}
+
+class _QuizzPageState extends State<QuizzPage> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+        if (int.parse(widget.numberController.text) > widget.questions.length) {
+          widget.numberController.text = widget.questions.length.toString();
+        }
+      },
+      child: Scaffold(
+          appBar: AppBar(title: const Text("Select number")),
+          body: BlocListener(
+            bloc: widget.cubit,
+            listener: (context, state) {
+              if (state is Start) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => Game(
+                          questions: state.questions.sublist(
+                              0, int.parse(widget.numberController.text)))),
+                );
+              }
+            },
+            child: SingleChildScrollView(
+              child: SizedBox(
+                height: height * 0.8,
+                width: width,
+                child: Stack(
+                  alignment: Alignment.topCenter,
+                  children: [
+                    Positioned(
+                      top: height * 0.3,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Number of questions",
+                            style: TextStyle(fontSize: height * 0.025),
+                          ),
+                          SizedBox(
+                            height: height * 0.03,
+                          ),
+                          SizedBox(
+                            width: width * 0.25,
+                            height: height * 0.07,
+                            child: TextField(
+                              onChanged: (String value) {},
+                              controller: widget.numberController,
+                              textAlign: TextAlign.center,
+                              keyboardType: TextInputType.number,
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: '0',
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: height * 0.03,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              ElevatedButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      widget.numberController.value =
+                                          TextEditingValue(
+                                              text: widget.questions.length
+                                                  .toString());
+                                      FocusManager.instance.primaryFocus
+                                          ?.unfocus();
+                                    });
+                                  },
+                                  child: const Text("Max"))
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        child: SizedBox(
+                          width: width * 0.6,
+                          height: height * 0.07,
+                          child: ElevatedButton(
+                              onPressed: (widget
+                                          .numberController.text.isEmpty ||
+                                      int.parse(widget.numberController.text) <
+                                          1)
+                                  ? null
+                                  : () {
+                                      widget.cubit.startQuizz(
+                                          widget.questions.shuffleAll);
+                                    },
+                              child: Text(
+                                "Start",
+                                style: TextStyle(fontSize: height * 0.025),
+                              )),
+                        ))
+                  ],
+                ),
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+/*class QuizzPage extends StatelessWidget {
   final List<Question> questions;
   final cubit = QuizzCubit();
   final numberController = TextEditingController();
@@ -17,7 +140,7 @@ class QuizzPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
     final height = MediaQuery.of(context).size.height;
-
+  
     return GestureDetector(
       onTap: () {
         FocusManager.instance.primaryFocus?.unfocus();
@@ -26,7 +149,7 @@ class QuizzPage extends StatelessWidget {
         }
       },
       child: Scaffold(
-          appBar: AppBar(title: Text("Select number")),
+          appBar: AppBar(title: const Text("Select number")),
           body: BlocListener(
             bloc: cubit,
             listener: (context, state) {
@@ -41,7 +164,7 @@ class QuizzPage extends StatelessWidget {
               }
             },
             child: SingleChildScrollView(
-              child: Container(
+              child: SizedBox(
                 height: height * 0.8,
                 width: width,
                 child: Stack(
@@ -67,8 +190,8 @@ class QuizzPage extends StatelessWidget {
                               controller: numberController,
                               textAlign: TextAlign.center,
                               keyboardType: TextInputType.number,
-                              decoration: InputDecoration(
-                                border: const OutlineInputBorder(),
+                              decoration: const InputDecoration(
+                                border: OutlineInputBorder(),
                                 hintText: '0',
                               ),
                             ),
@@ -81,10 +204,12 @@ class QuizzPage extends StatelessWidget {
                             children: [
                               ElevatedButton(
                                   onPressed: () {
-                                    numberController.text =
-                                        questions.length.toString();
+                                    numberController.value = TextEditingValue(
+                                        text: questions.length.toString());
+                                    FocusManager.instance.primaryFocus
+                                        ?.unfocus();
                                   },
-                                  child: Text("Max"))
+                                  child: const Text("Max"))
                             ],
                           ),
                         ],
@@ -114,4 +239,4 @@ class QuizzPage extends StatelessWidget {
           )),
     );
   }
-}
+}*/
